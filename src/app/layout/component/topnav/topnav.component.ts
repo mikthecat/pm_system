d173b715340edcd9b65df83f20c08153fcb75ee4
mikeila <mikmikviernes@gmail.com>
2024-01-08 19:13:38 +0800

    FINAL

--- src/app/layout/component/topnav/topnav.component.ts
+++ src/app/layout/component/topnav/topnav.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topnav',
   templateUrl: './topnav.component.html',
-  styleUrls: ['./topnav.component.scss']
+  styleUrls: ['./topnav.component.scss'],
 })
 export class TopnavComponent {
+  constructor(private router: Router) {}
+  isAdmin: boolean = false;
+  isLoggedIn: boolean = false;
+  isDoctor: boolean = false;
+  isUser: boolean = false;
 
+  ngOnInit() {
+    const sessionString = localStorage.getItem('session');
+    const session = JSON.parse(sessionString as string);
+
+    if (session.isAdmin) {
+      this.isAdmin = true;
+    }
+
+    if (session.isDoctor) {
+      this.isDoctor = true;
+    }
+
+    if (session.ok) {
+      this.isLoggedIn = true;
+    }
+
+    if (this.isAdmin === false && this.isDoctor === false) {
+      this.isUser = true;
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('session');
+    alert('User logged out');
+    this.router.navigateByUrl('/login');
+  }
 }
--- src/app/layout/patient-management/patient-management.component.ts
+++ src/app/layout/patient-management/patient-management.component.ts
@@ -7,27 +7,188 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-patient-management',
   templateUrl: './patient-management.component.html',
-  styleUrls: ['./patient-management.component.scss']
+  styleUrls: ['./patient-management.component.scss'],
 })
 export class PatientManagementComponent {
-  @ViewChild('f') loginForm !: NgForm
-  patients!: patient[]
-  patientSubscription!: Subscription
+  @ViewChild('f') loginForm!: NgForm;
+  @ViewChild('patientForm') patientForm!: NgForm;
+  patients!: patient[];
+  currentPatient: patient = {
+    idpatients: 0,
+    name: '',
+    age: 0,
+    sex: '',
+    patient_number: 0,
+    appointment: '',
+    doctor: 0,
+    admission_date: '',
+    discharged_date: '',
+    doctors: {
+      iddoctors: 0,
+      name: '',
+      age: 0,
+      sex: '',
+      specialist: '',
+      date_hired: '',
+    },
+  };
 
-  constructor(private patientService: PatientService){}
+  updatePatient: patient = {
+    idpatients: 0,
+    name: '',
+    age: 0,
+    sex: '',
+    patient_number: 0,
+    appointment: '',
+    doctor: 0,
+    admission_date: '',
+    discharged_date: '',
+    doctors: {
+      iddoctors: 0,
+      name: '',
+      age: 0,
+      sex: '',
+      specialist: '',
+      date_hired: '',
+    },
+  };
+  dischargedDate: string = '';
+  admissionDate: string = '';
+  patientSubscription!: Subscription;
+  currentPatientId: number | null = null;
+
+  filteredPatients: patient[] = [];
+  searchTerm: string = '';
+  constructor(private patientService: PatientService) {
+    this.filteredPatients = this.patients;
+  }
 
   ngOnInit(): void {
-    this.patientSubscription = this.patientService.patients.subscribe((data) => {
-      this.patients = data
-    })
+    // this.patientSubscription = this.patientService.patients.subscribe(
+    //   (data) => {
+    //     this.patients = data;
+    //   }
+    // );
+
+    const getPatients = async () => {
+      const res = await fetch('http://localhost:8000/api/patients');
+      const data = await res.json();
+
+      console.log(data);
+      this.patients = data.patients;
+      this.filteredPatients = this.patients;
+    };
+
+    getPatients();
   }
 
-  ngOnDestroy(): void {
-    this.patientSubscription.unsubscribe()
+  // ngOnDestroy(): void {
+  //   this.patientSubscription.unsubscribe();
+  // }
+  onSearch(): void {
+    this.filteredPatients = this.searchTerm
+      ? this.patients.filter((patient) =>
+          patient.patient_number
+            .toString()
+            .toLowerCase()
+            .includes(this.searchTerm.toString().toLowerCase())
+        )
+      : this.patients;
   }
-  onSubmit(){
+
+  // onSearch(): void {
+  //   this.filteredPatients = this.searchTerm
+  //     ? this.patients.filter(
+  //         (patient) => patient.patient_number === Number(this.searchTerm)
+  //       )
+  //     : this.patients;
+  // }
+
+  async onSubmit() {
+    function generatePatientNumber(existingNumbers: number[]): number {
+      const MAX_PATIENT_NUMBER = 2147483647;
+      let newPatientNumber: number;
+
+      do {
+        newPatientNumber = Math.floor(Math.random() * MAX_PATIENT_NUMBER) + 1;
+      } while (existingNumbers.includes(newPatientNumber));
+
+      return newPatientNumber;
+    }
+
+    // Usage example:
+    const existingPatientNumbers = this.patients.map(
+      (patient) => patient.patient_number
+    );
+
     console.log(this.loginForm.value);
-    this.patientService.addSubject(this.loginForm.value.name, this.loginForm.value.age, this.loginForm.value.gender, this.loginForm.value.patientNumber, this.loginForm.value.admissionDate, this.loginForm.value.dischargedDate)
-    this.loginForm.reset()
+    this.loginForm.value.patient_number = generatePatientNumber(
+      existingPatientNumbers
+    );
+    console.log(this.loginForm.value.patient_number);
+
+    const res = await fetch('http://localhost:8000/api/patients', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(this.loginForm.value),
+    });
+
+    const data = await res.json();
+
+    console.log(data);
+    this.ngOnInit();
+    // this.patientService.addSubject(
+    //   this.loginForm.value.name,
+    //   this.loginForm.value.age,
+    //   this.loginForm.value.gender,
+    //   this.loginForm.value.patientNumber,
+    //   this.loginForm.value.admissionDate,
+    //   this.loginForm.value.dischargedDate
+    // );
+    // this.loginForm.reset();
+  }
+
+  setPatientId(id: number) {
+    this.currentPatientId = id;
+
+    console.log(this.currentPatientId);
+  }
+
+  setCurrentPatient(patient: patient) {
+    this.currentPatient = patient;
+    this.updatePatient = { ...this.currentPatient };
+    this.setPatientId(patient.idpatients);
+  }
+
+  async updatePatientSubmit() {
+    console.log(this.patientForm.value);
+    const res = await fetch(
+      `http://localhost:8000/api/patients/${this.currentPatientId}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(this.patientForm.value),
+      }
+    );
+
+    const data = await res.json();
+    this.ngOnInit();
+    console.log(data);
+  }
+
+  async deletePatient() {
+    const res = await fetch(
+      `http://localhost:8000/api/patients/${this.currentPatientId}`,
+      {
+        method: 'DELETE',
+      }
+    );
+    const data = await res.json();
+    this.ngOnInit();
+    console.log(data);
   }
 }
